7f46639ee991ffac8b659dbe287c488aaaef1f2c
Yegor <belgeomax@gmail.com>
2025-11-03 00:22:25 +0000

    social icons

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,4 +1,6 @@
 export const SOCIALS_BLUESKY = 'https://bsky.app/profile/y390r.bsky.social'
+export const SOCIALS_LINKEDIN = 'https://www.linkedin.com/in/yegor-belov-668911a2/'
 export const SOCIALS_CHESS = 'https://link.chess.com/friend/6HQ5Ez'
+export const SOCIALS_GITHUB = 'https://github.com/bel0v'
 export const PUBLIC_PGP_FILE =
   'https://keys.openpgp.org/vks/v1/by-fingerprint/32F72A8C2B122986180DC76A3ADF3D1B7248B4FB'
